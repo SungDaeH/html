d67e0d77ab1ae04c8e395a6ab084a90dea6972d9
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-04-24 11:48:28 +0900

    0424

---
+++ 30918_황성대_0424/js/javascript.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function(){
+    $(".navi>li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown();
+    }).mouseout(function(){
+        $(this).find(".submenu").stop().slideUp();
+    })
+})
