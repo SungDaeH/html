918b1686fccc9763f6e77dd20e5cb65845f7e018
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-05-08 10:53:15 +0900

    완성

--- ON쇼핑몰/JavaScript/script.js
+++ ON쇼핑몰/JavaScript/script.js
@@ -16,3 +16,17 @@ setInterval(function(){
     .next('a').fadeIn()
     .end().appendTo('.imgslide');
 }, 3000)
+
+$(function(){
+    $(".tabmenu>li>a").click(function(){
+        $(this).parent().addClass('active').siblings().removeClass('active');
+        return false;
+    });
+})
+
+$(".notice li:first").click(function(){
+    $("#modal").addClass("active");
+});
+$(".btn").click(function(){
+    $("#modal").removeClass("active");
+});
