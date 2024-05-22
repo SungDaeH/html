fb946a8624fe26d208bd9bb542e902b37cdcd1ce
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-05-22 11:54:45 +0900

    Add files via upload

---
+++ 30918_황성대_0522/javascript/script.js
@@ -0,0 +1,24 @@
+$(".main>li").mouseover(function () {
+    $(this).find(".sub").stop().slideDown();
+});
+
+$(".main>li").mouseout(function () {
+    $(this).find(".sub").stop().slideUp();
+});
+
+setInterval(function () {
+    $('.slidelist').delay(2000);
+    $('.slidelist').animate({ marginLeft: -1200 })
+    $('.slidelist').delay(2000);
+    $('.slidelist').animate({ marginLeft: -2400 })
+    $('.slidelist').delay(2000);
+    $('.slidelist').animate({ marginLeft: -3600 })
+    $('.slidelist').delay(2000);
+    $('.slidelist').animate({ marginLeft: 0 })
+    $('.slidelist').delay(2000);
+})
+
+$(".tabmenu>li>a").click(function () {
+    $(this).parent().addClass('active').siblings().removeClass('active');
+    return false;
+})
