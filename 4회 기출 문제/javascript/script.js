ecb7b1a1fc4ce949e47cdf656b02cb912a8de3ab
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-05-28 11:51:20 +0900

    0528

--- 4회 기출 문제/javascript/script.js
+++ 4회 기출 문제/javascript/script.js
@@ -5,3 +5,18 @@ $(".navi>li").mouseover(function(){
 $(".navi>li").mouseout(function(){
     $(this).find('.sub').stop().slideUp();
 });
+
+setInterval(function(){
+    $('.slidelist').delay(3000);
+    $('.slidelist').animate({marginTop : -300});
+    $('.slidelist').delay(3000);
+    $('.slidelist').animate({marginTop : -600});
+    $('.slidelist').delay(3000);
+    $('.slidelist').animate({marginTop : 0});
+    $('.slidelist').delay(3000);
+})
+
+$(".tabmenu>li>a").click(function(){
+    $(this).parent().addClass('active').siblings().removeClass('active');
+    return false;
+});
