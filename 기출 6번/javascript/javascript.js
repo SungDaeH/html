c4173d3ae7e2f2d0790c0280851ddb850c6c5d3f
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-05-29 12:42:50 +0900

    0529

--- 기출 6번/javascript/javascript.js
+++ 기출 6번/javascript/javascript.js
@@ -4,3 +4,26 @@ $(".navi>li").mouseover(function(){
 $(".navi>li").mouseout(function(){
     $(this).find('.sub').stop().slideUp();
 });
+
+setInterval(function(){
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({marginLeft:-1200});
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({marginLeft:-2400});
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({marginLeft:0});
+    $(".slidelist").delay(2000);
+})
+
+$('.tabmenu>li>a').click(function(){
+    $(this).parent().addClass("active").siblings().removeClass("active");
+    return false;
+})
+
+$(".notice li:first").click(function(){
+    $("#modal").addClass('active');
+});
+
+$(".btn").click(function(){
+    $("#modal").removeClass('active');
+});
