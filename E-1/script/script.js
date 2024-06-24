05f168c6fb96a10c4c59bc09154480a1dea0d8ba
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-06-24 15:43:54 +0900

    Add files via upload

--- E-1/script/script.js
+++ E-1/script/script.js
@@ -6,4 +6,17 @@ $(".nav>li").mouseout(function(){
     $(this).find(".sub").stop().slideUp();
 });
 
+var i = 0;
 
+function slidelist(){
+    if(i<2){
+        i++;
+    }else{
+        i=0;
+    }
+
+    $(".slidelist ul li").fadeOut();
+    $(".slidelist ul li").eq(i).fadeIn();
+}
+
+setInterval(slidelist, 3000)
