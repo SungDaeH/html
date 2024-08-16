29dfdcf74e1e63464bc6922c7a581eba5c0a5824
황성대 <121483266+SungDaeH@users.noreply.github.com>
2024-08-16 12:46:35 +0900

    응프

---
+++ home/output.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>결과확인</title>
+</head>
+
+<body>
+    아이디 :<?php echo $_GET["uid"]; ?><br>
+    비밀번호 :<?php echo $_GET["upwd"]; ?><br>
+    졸업년도 :<?php echo $_GET["uyear"]; ?><br>
+    학번 :<?php echo $_GET["uhakbun"]; ?><br>
+    폰번호 :<?php echo $_GET["utel"]; ?><br>
+    이메일 :<?php echo $_GET["uemail"]; ?><br>
+    집주소 :<?php echo $_GET["uaddr"]; ?><br>
+    기타 :<?php echo $_GET["uetc"]; ?><br>
+</body>
+
+</html>
